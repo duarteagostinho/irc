4204e7bba5be150ad9ad835836fd315077c6e5af
vloureir <vloureir@student.42lisboa.com>
2026-06-12 18:59:05 +0100

    privmsg edge cases

--- src/Commands.cpp
+++ src/Commands.cpp
@@ -6,7 +6,7 @@
 /*   By: vloureir <vloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 15:37:18 by vloureir          #+#    #+#             */
-/*   Updated: 2026/06/12 17:46:24 by vloureir         ###   ########.fr       */
+/*   Updated: 2026/06/12 18:58:28 by vloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ void Server::join(std::vector<std::string>& av, int index)
 				std::string response = "Cannot join " + av[1] + " (Requires keyword)\r\n";
 				send(_fds[index].fd, response.c_str(), response.size() + 1, 0);
 			}
-			else // If invited, no pass or pass was correct, enter channel
+			else  // If invited, no pass or pass was correct, enter channel
 			{
 				_channels[i].addUser(_users[index].getNickname(), 0);
 
@@ -448,70 +448,6 @@ void Server::broadcastMessage(std::string message, int index, int flag)
 
 }
 
-void	Server::privmsg(std::vector<std::string>& av, int index)
-{
-	(void)av;
-	(void)index;
-
-	// av[0] = whole line
-	// av[1] = target,{target}
-	// av[2+] = rest of the message (NOT USED)
-
-	std::vector<std::string> targets;
-
-	if (av.size() == 1) // Missing arguments
-	{
-		std::string response = ":server 411 " + _users[index].getNickname() + " :No recipient given (PRIVMSG)\r\n";
-		send(_fds[index].fd, response.c_str(), response.size(), 0);
-		return ;
-	}
-	// Split the channels argument and loop through them to check every channel received
-	splitString(av[1], targets, ',');
-	for (size_t i = 0; i < targets.size(); i++)
-	{
-		if (!isChannel(targets[i]))
-		{
-			std::string response = ":server 403 " + _users[index].getNickname() + " " + targets[i] + " :No such channel\r\n";
-			send(_fds[index].fd, response.c_str(), response.size(), 0);
-		}
-		else if (av.size() == 2) // Print topic
-		{
-			if (_channels[getChannelIndex(targets[i])].getTopic() == "")
-			{
-				std::string response =":server 331 " + _users[index].getNickname() + " " + targets[i] + " :No topic is set\r\n";
-				send(_fds[index].fd, response.c_str(), response.size(), 0);
-			}
-			else
-			{
-				std::string response = ":server 332 " + _users[index].getNickname() + " " + targets[i] + " :" + _channels[getChannelIndex(targets[i])].getTopic() + "\r\n";
-				send(_fds[index].fd, response.c_str(), response.size(), 0);
-				
-				response = ":server 333 " + _users[index].getNickname() + " " + targets[i] + " " + getClientInfo(index) + " 0\r\n"; // timestamp ex: 1781075528	
-				send(_fds[index].fd, response.c_str(), response.size(), 0);
-			}
-		}
-		else
-		{
-			std::string msg = craftStringSpaces(av[0], 2);
-//			std::cout << new_topic << std::endl;
-			// BROADCAST TO THE WHOLE CHANNEL THE NEW TOPIC
-			int ch_id = getChannelIndex(av[1]);
-			for (size_t u = 0; u < _users.size(); u++)
-			{
-				if (_users[u].getNickname() == _users[index].getNickname())
-					continue;
-				if (_channels[ch_id].isUserOnChannel(_users[u].getNickname()))
-				{
-					std::string response = getClientInfo(index) + " PRIVMSG " + av[1] + " :" + msg + "\r\n";
-					send(_fds[u].fd, response.c_str(), response.size(), 0);
-				}
-			}
-		}
-	}
-
-}
-
-
 // void	Server::privmsg(std::vector<std::string>& av, int index)
 // {
 // 	(void)av;
@@ -572,127 +508,69 @@ void	Server::privmsg(std::vector<std::string>& av, int index)
 // 			}
 // 		}
 // 	}
+
 // }
 
-/*
+
+void	Server::privmsg(std::vector<std::string>& av, int index)
+{
+	// av[0] = whole line
+	// av[1] = target,{target}
+	// av[2+] = rest of the message (NOT USED)
+	
 	std::vector<std::string> targets;
 
-	if (size == 1)
-		411 chaud :No recipient given (PRIVMSG)
-	else if (size == 2)
-		412 chaud :No text to send
- 
-
+	if (av.size() < 3) // Missing arguments
+	{
+		std::string response;
+		if (av.size() == 1)
+			response = ":server 411 " + _users[index].getNickname() + " :No recipient given (PRIVMSG)\r\n";
+		else
+			response = ":server 412 " + _users[index].getNickname() + " :No text to send\r\n";
+		send(_fds[index].fd, response.c_str(), response.size(), 0);
+		return ;
+	}
+	
 	splitString(av[1], targets, ',');
-
+	std::string response;
 	for (size_t i = 0; i < targets.size(); i++)
 	{
-		bool channel = isChannel(targets[i];
-
-		if ((targets.size() > 1 && channel == true) || not on channel)
-			404 chaud #42 :Cannot send to channel
-		else if (targets[i] dont exists)
-			401 chaud user_1 :No such nick
-		else if (channel == false)
-			403 chaud #42 :No such channel
-		else
-			:user_5!user_5@1C72DB:D17C90:7D2707:482FF6:IP PRIVMSG chaud :hi hi // RECEIVER GETS THIS
-	
-	
-
-
-	
+		bool channel = isChannel(targets[i]);
+		if ((targets.size() > 1 && channel == true)
+			|| !_channels[getChannelIndex(targets[i])].isUserOnChannel(_users[index].getNickname()))
+				response = ":server 404 " + _users[index].getNickname() + " " + targets[i] + " :Cannot send to channel\r\n";
+//				404 chaud #42 :Cannot send to channel
+			else if (!doesUserExist(targets[i]))
+				response = ":server 401 " + _users[index].getNickname() + " " + targets[i] + " :No such nick\r\n";
+//				401 chaud user_1 :No such nick
+			else if (channel == false)
+				response = ":server 403 " + _users[index].getNickname() + " " + targets[i] + " :No such channel\r\n";
+//				403 chaud #42 :No such channel
+			
 	}
+	// 	if (size == 1)
+	// 	411 chaud :No recipient given (PRIVMSG)
+	// else if (size == 2)
+	// 	412 chaud :No text to send
+ 
+
+	// splitString(av[1], targets, ',');
+
+	// for (size_t i = 0; i < targets.size(); i++)
+	// {
+	// 	bool channel = isChannel(targets[i];
+
+	// 	if ((targets.size() > 1 && channel == true) || not on channel)
+	// 		404 chaud #42 :Cannot send to channel
+	// 	else if (targets[i] dont exists)
+	// 		401 chaud user_1 :No such nick
+	// 	else if (channel == false)
+	// 		403 chaud #42 :No such channel
+	// 	else
+	// 		:user_5!user_5@1C72DB:D17C90:7D2707:482FF6:IP PRIVMSG chaud :hi hi // RECEIVER GETS THIS
+
 
 	
 
-*/
-
-
-
-/*
-
-PRIVMSG
-:Aurora.AfterNET.Org 411 chaud :No recipient given (PRIVMSG)
-JOIN
-:Aurora.AfterNET.Org 461 chaud JOIN :Not enough parameters
-PRIVMSg #42
-:Aurora.AfterNET.Org 412 chaud :No text to send
-PRIVMSG #42 hi
-:Aurora.AfterNET.Org 403 chaud #42 :No such channel
-PRIVMSg #42,#11
-:Aurora.AfterNET.Org 412 chaud :No text to send
-PRIVMSG #42 hi
-:Aurora.AfterNET.Org 404 chaud #42 :Cannot send to channel
-JOIN #42
-:chaud!vini@AN-EA7BE6BE.net.novis.pt JOIN #42
-:Aurora.AfterNET.Org 353 chaud = #42 :chaud @user_5
-:Aurora.AfterNET.Org 366 chaud #42 :End of /NAMES list.
-PRIVMSG #42,#11 hello
-:Aurora.AfterNET.Org 404 chaud #42 :Cannot send to channel
-:Aurora.AfterNET.Org 404 chaud #11 :Cannot send to channel
-PRIVMSG #42 hi
-:No recipient given (PRIVMSG)
-:Aurora.AfterNET.Org 421 chaud recipient :Unknown command
-PRIVMSG user_5 hello
-PRIVMSG user_5 good morning wake up
-:user_5!user_5@1C72DB:D17C90:7D2707:482FF6:IP PRIVMSG chaud :hi hi
-PRIVMSG useee hello there
-:Aurora.AfterNET.Org 401 chaud useee :No such nick
-^C
-vloureir@Vinicius:~$ nc -C irc.afternet.org 6667
-NOTICE * :*** Looking up your hostname
-NOTICE * :*** Found your hostname
-USER vini 0 * :vini
-NICK vini
-PING :1778091801
-PONG 1778091801
-NOTICE * :*** Checking your client version (sorry..)
-
-PRIVMSG <nick> :VERSION
-001 <nick> 
-
-
-:luna.AfterNET.Org PRIVMSG vini :VERSION
-:luna.AfterNET.Org 001 vini :Welcome to the AfterNET IRC Network, vini
-:luna.AfterNET.Org 002 vini :Your host is luna.AfterNET.Org, running version u2.10.12.14+Nefarious(2.0.0)
-:luna.AfterNET.Org 003 vini :This server was created Fri May 29 2026 at 23:21:43 UTC
-:luna.AfterNET.Org 004 vini luna.AfterNET.Org u2.10.12.14+Nefarious(2.0.0) adgiknoqswxzBDHLNORWX abCcDdhikLlMmNnOopQRrSsTtvZz bhkLlov
-:luna.AfterNET.Org 005 vini WHOX WALLCHOPS WALLHOPS WALLVOICES USERIP CPRIVMSG CNOTICE NAMESX UHNAMES SILENCE=25 WATCH=128 MODES=6 MAXCHANNELS=50 :are supported by this server
-:luna.AfterNET.Org 005 vini MAXBANS=50 NICKLEN=30 MAXNICKLEN=30 TOPICLEN=250 AWAYLEN=250 KICKLEN=250 CHANNELLEN=100 MAXCHANNELLEN=200 CHANTYPES=# PREFIX=(ohv)@%+ STATUSMSG=@%+ BOT=B CHANMODES=be,k,Ll,aCcDdiMmNnOpQRrSsTtZz :are supported by this server
-:luna.AfterNET.Org 005 vini CASEMAPPING=rfc1459 NETWORK=AfterNET MAXLIST=b:50,e:45 ELIST=CT EXCEPTS=e MAXEXCEPTS=45 EXTBANS=~,acjnqrmM :are supported by this server
-
-
-
-:luna.AfterNET.Org 251 vini :There are 212 users and 93 invisible on 9 servers
-:luna.AfterNET.Org 252 vini 23 :operator(s) online
-:luna.AfterNET.Org 254 vini 181 :channels formed
-:luna.AfterNET.Org 255 vini :I have 97 clients and 1 servers
-:luna.AfterNET.Org 265 vini :Current local users: 97 Max: 258
-:luna.AfterNET.Org 266 vini :Current global users: 305 Max: 568
-:luna.AfterNET.Org NOTICE vini :Highest connection count: 108 (107 clients)
-:luna.AfterNET.Org 422 vini :MOTD File is missing
-:luna.AfterNET.Org NOTICE vini :on 1 ca 1(6) ft 10(10)
-:luna.AfterNET.Org 396 vini AN-EA7BE6BE.net.novis.pt :is now your hidden host
-:vini!vini@AN-EA7BE6BE.net.novis.pt MODE vini +x
-JOIN #42
-:vini!vini@AN-EA7BE6BE.net.novis.pt JOIN #42
-:luna.AfterNET.Org 353 vini = #42 :vini @user_5
-:luna.AfterNET.Org 366 vini #42 :End of /NAMES list.
-JOIN #11
-:vini!vini@AN-EA7BE6BE.net.novis.pt JOIN #11
-:luna.AfterNET.Org 353 vini = #11 :vini @user_5
-:luna.AfterNET.Org 366 vini #11 :End of /NAMES list.
-PRIVMSG #42,#11,user_5 :wake up
-:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
-:luna.AfterNET.Org 404 vini #11 :Cannot send to channel
-PRIVMSG user_5,user_66 :wakey wakey
-PRIVMSG #11 hi
-PRIVMSG #42,#11 alo
-:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
-:luna.AfterNET.Org 404 vini #11 :Cannot send to channel
-PRIVMSG #42 alo
-PRIVMSG #42,user_5,user_66 hello there
-:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
-
-*/
+	
+}
--- src/cmd.md
+++ src/cmd.md
@@ -1,4 +1,8 @@
 
+PART #a
+:vini!vini@740E9B.95C97E.C247D8.AE513.IP PART :#a
+
+
 
 ///////////////////////////////////////////////////			TOPIC		///////////////////////////////////////////////////
 
@@ -179,4 +183,148 @@ JOIN #b
 :Aurora.AfterNET.Org 366 alo #b :End of /NAMES list.
 INVITE chaud #a,#b
 :Aurora.AfterNET.Org 403 alo #a,#b :No such channel
+*/
+
+///////////////////////////////////////////////////			PRIVMSG		///////////////////////////////////////////////////
+
+
+
+/*
+	std::vector<std::string> targets;
+
+	if (size == 1)
+		411 chaud :No recipient given (PRIVMSG)
+	else if (size == 2)
+		412 chaud :No text to send
+ 
+
+	splitString(av[1], targets, ',');
+
+	for (size_t i = 0; i < targets.size(); i++)
+	{
+		bool channel = isChannel(targets[i];
+
+		if ((targets.size() > 1 && channel == true) || not on channel)
+			404 chaud #42 :Cannot send to channel
+		else if (targets[i] dont exists)
+			401 chaud user_1 :No such nick
+		else if (channel == false)
+			403 chaud #42 :No such channel
+		else
+			:user_5!user_5@1C72DB:D17C90:7D2707:482FF6:IP PRIVMSG chaud :hi hi // RECEIVER GETS THIS
+
+	}
+
+*/
+
+
+
+/*
+
+PRIVMSG
+:Aurora.AfterNET.Org 411 chaud :No recipient given (PRIVMSG)
+JOIN
+:Aurora.AfterNET.Org 461 chaud JOIN :Not enough parameters
+PRIVMSg #42
+:Aurora.AfterNET.Org 412 chaud :No text to send
+PRIVMSG #42 hi
+:Aurora.AfterNET.Org 403 chaud #42 :No such channel
+PRIVMSg #42,#11
+:Aurora.AfterNET.Org 412 chaud :No text to send
+PRIVMSG #42 hi
+:Aurora.AfterNET.Org 404 chaud #42 :Cannot send to channel
+JOIN #42
+:chaud!vini@AN-EA7BE6BE.net.novis.pt JOIN #42
+:Aurora.AfterNET.Org 353 chaud = #42 :chaud @user_5
+:Aurora.AfterNET.Org 366 chaud #42 :End of /NAMES list.
+PRIVMSG #42,#11 hello
+:Aurora.AfterNET.Org 404 chaud #42 :Cannot send to channel
+:Aurora.AfterNET.Org 404 chaud #11 :Cannot send to channel
+PRIVMSG #42 hi
+:No recipient given (PRIVMSG)
+:Aurora.AfterNET.Org 421 chaud recipient :Unknown command
+PRIVMSG user_5 hello
+PRIVMSG user_5 good morning wake up
+:user_5!user_5@1C72DB:D17C90:7D2707:482FF6:IP PRIVMSG chaud :hi hi
+PRIVMSG useee hello there
+:Aurora.AfterNET.Org 401 chaud useee :No such nick
+^C
+vloureir@Vinicius:~$ nc -C irc.afternet.org 6667
+NOTICE * :*** Looking up your hostname
+NOTICE * :*** Found your hostname
+USER vini 0 * :vini
+NICK vini
+PING :1778091801
+PONG 1778091801
+NOTICE * :*** Checking your client version (sorry..)
+
+PRIVMSG <nick> :VERSION
+001 <nick> 
+
+
+:luna.AfterNET.Org PRIVMSG vini :VERSION
+:luna.AfterNET.Org 001 vini :Welcome to the AfterNET IRC Network, vini
+:luna.AfterNET.Org 002 vini :Your host is luna.AfterNET.Org, running version u2.10.12.14+Nefarious(2.0.0)
+:luna.AfterNET.Org 003 vini :This server was created Fri May 29 2026 at 23:21:43 UTC
+:luna.AfterNET.Org 004 vini luna.AfterNET.Org u2.10.12.14+Nefarious(2.0.0) adgiknoqswxzBDHLNORWX abCcDdhikLlMmNnOopQRrSsTtvZz bhkLlov
+:luna.AfterNET.Org 005 vini WHOX WALLCHOPS WALLHOPS WALLVOICES USERIP CPRIVMSG CNOTICE NAMESX UHNAMES SILENCE=25 WATCH=128 MODES=6 MAXCHANNELS=50 :are supported by this server
+:luna.AfterNET.Org NOTICE vini :Highest connection count: 108 (107 clients)
+:luna.AfterNET.Org 422 vini :MOTD File is missing
+:luna.AfterNET.Org NOTICE vini :on 1 ca 1(6) ft 10(10)
+:luna.AfterNET.Org 396 vini AN-EA7BE6BE.net.novis.pt :is now your hidden host
+:vini!vini@AN-EA7BE6BE.net.novis.pt MODE vini +x
+JOIN #42
+:vini!vini@AN-EA7BE6BE.net.novis.pt JOIN #42
+:luna.AfterNET.Org 353 vini = #42 :vini @user_5
+:luna.AfterNET.Org 366 vini #42 :End of /NAMES list.
+JOIN #11
+:vini!vini@AN-EA7BE6BE.net.novis.pt JOIN #11
+:luna.AfterNET.Org 353 vini = #11 :vini @user_5
+:luna.AfterNET.Org 366 vini #11 :End of /NAMES list.
+PRIVMSG #42,#11,user_5 :wake up
+:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
+:luna.AfterNET.Org 404 vini #11 :Cannot send to channel
+PRIVMSG user_5,user_66 :wakey wakey
+PRIVMSG #11 hi
+PRIVMSG #42,#11 alo
+:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
+:luna.AfterNET.Org 404 vini #11 :Cannot send to channel
+PRIVMSG #42 alo
+PRIVMSG #42,user_5,user_66 hello there
+:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
+
+:luna.AfterNET.Org 005 vini MAXBANS=50 NICKLEN=30 MAXNICKLEN=30 TOPICLEN=250 AWAYLEN=250 KICKLEN=250 CHANNELLEN=100 MAXCHANNELLEN=200 CHANTYPES=# PREFIX=(ohv)@%+ STATUSMSG=@%+ BOT=B CHANMODES=be,k,Ll,aCcDdiMmNnOpQRrSsTtZz :are supported by this server
+:luna.AfterNET.Org 005 vini CASEMAPPING=rfc1459 NETWORK=AfterNET MAXLIST=b:50,e:45 ELIST=CT EXCEPTS=e MAXEXCEPTS=45 EXTBANS=~,acjnqrmM :are supported by this server
+
+:luna.AfterNET.Org 251 vini :There are 212 users and 93 invisible on 9 servers
+:luna.AfterNET.Org 252 vini 23 :operator(s) online
+:luna.AfterNET.Org 254 vini 181 :channels formed
+:luna.AfterNET.Org 255 vini :I have 97 clients and 1 servers
+:luna.AfterNET.Org 265 vini :Current local users: 97 Max: 258
+:luna.AfterNET.Org 266 vini :Current global users: 305 Max: 568
+:luna.AfterNET.Org NOTICE vini :Highest connection count: 108 (107 clients)
+:luna.AfterNET.Org 422 vini :MOTD File is missing
+:luna.AfterNET.Org NOTICE vini :on 1 ca 1(6) ft 10(10)
+:luna.AfterNET.Org 396 vini AN-EA7BE6BE.net.novis.pt :is now your hidden host
+:vini!vini@AN-EA7BE6BE.net.novis.pt MODE vini +x
+JOIN #42
+:vini!vini@AN-EA7BE6BE.net.novis.pt JOIN #42
+:luna.AfterNET.Org 353 vini = #42 :vini @user_5
+:luna.AfterNET.Org 366 vini #42 :End of /NAMES list.
+JOIN #11
+:vini!vini@AN-EA7BE6BE.net.novis.pt JOIN #11
+:luna.AfterNET.Org 353 vini = #11 :vini @user_5
+:luna.AfterNET.Org 366 vini #11 :End of /NAMES list.
+PRIVMSG #42,#11,user_5 :wake up
+:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
+:luna.AfterNET.Org 404 vini #11 :Cannot send to channel
+PRIVMSG user_5,user_66 :wakey wakey
+PRIVMSG #11 hi
+PRIVMSG #42,#11 alo
+:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
+:luna.AfterNET.Org 404 vini #11 :Cannot send to channel
+PRIVMSG #42 alo
+PRIVMSG #42,user_5,user_66 hello there
+:luna.AfterNET.Org 404 vini #42 :Cannot send to channel
+
 */
